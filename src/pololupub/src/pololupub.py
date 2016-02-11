f4bfafa7002bab0ff7d38becf669ae3719ed1f3e
Chu-Sheng Ku <chusheng.ku@colorado.edu>
2016-02-11 16:51:47 +0000

    Remove logs

--- src/pololupub/src/pololupub.py
+++ src/pololupub/src/pololupub.py
@@ -5,16 +5,17 @@ import rospy
 from std_msgs.msg import Float64
 
 
-def callback():
-    rospy.loginfo("I heard PID")
-    pwn = data.data
+def callback(data):
+    rospy.loginfo("I heard PID: " + str(data.data))
+    pwm = int(data.data)
     pololu = maestro.Controller()
-    pololu.setTarget(0, 6000-pwn)
+    pololu.setAccel(0, 10)
+    pololu.setTarget(0, 6000-pwm)
 
 def subscriber():
     rospy.init_node('pololu_sub', anonymous=True)
 
-    rospy.Subscriber("Control_Effort", Float64, callback)
+    rospy.Subscriber("control_effort", Float64, callback)
                                                                          
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
@@ -24,9 +25,8 @@ if __name__ == '__main__':
     try:
         pololu = maestro.Controller()
         pololu.setSpeed(1, 2000)
-        pololu.setTarget(1, 6600)
-        print 'motor'
+        pololu.setTarget(1, 5000)
         subscriber()
 
-    except rospy.ROSInterruptExecution:
+    except rospy.ROSInterruptException:
         pass
--- src/state_estimate/scripts/state_publisher.py
+++ src/state_estimate/scripts/state_publisher.py
@@ -5,20 +5,28 @@ from std_msgs.msg import Float64
 
 CHANNEL_L_IR = 6
 CHANNEL_R_IR = 11
+WINDOW_SIZE = 10
 
 def publisher():
     pub = rospy.Publisher('state', Float64, queue_size=10)
     rospy.init_node('state_estimate', anonymous=True)
     rate = rospy.Rate(50)
     pololu = maestro.Controller()
+    window = [0] * WINDOW_SIZE
+    index = 0
     while not rospy.is_shutdown():
         left_dist = pololu.getPosition(CHANNEL_L_IR)
-        print 'left: ' + str(left_dist)
+        # print 'left: ' + str(left_dist)
         right_dist = pololu.getPosition(CHANNEL_R_IR)
-        print 'right: ' + str(right_dist)
+        # print 'right: ' + str(right_dist)
         diff = left_dist - right_dist
-        pub.publish(diff)
-        print diff
+        window[index] = diff
+        index += 1
+        index %= WINDOW_SIZE
+        average = sum(window) / WINDOW_SIZE
+        pub.publish(average)
+        # print 'diff: ' + str(diff)
+        # print 'average: ' + str(average)
         rate.sleep()
 
 if __name__ == '__main__':
