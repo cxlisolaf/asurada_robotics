56e4b740467181aa1fe67c11d3a6d62f60468e12
Chu-Sheng Ku <chusheng.ku@colorado.edu>
2018-03-17 22:24:40 +0000

    add pololu publisher

---
+++ src/pololupub/src/pololupub.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import maestro
+import rospy
+from std_msgs.msg import Float64
+
+
+def callback():
+    rospy.loginfo("I heard PID")
+    pwn = data.data
+    publisher(pid)
+
+
+def publisher(pid):
+    pub = rospy.Publisher('pololu', Float64, queue_size=10)
+    rospy.init_node('pololu_pub', anonymous=True)
+    rate = rospy.Rate(10)
+    pololu = maestro.Controller()
+    while not rospy.is_shutdown():
+        pub.publish(pwn)
+        rate.sleep()
+
+                  
+def subscriber():
+    rospy.init_node('pololu_sub', anonymous=True)
+
+    rospy.Subscriber("Control_Effort", Float64, callback)
+                                                                         
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+                                                                                             
+
+if __name__ == '__main__':
+    try:
+        subscriber()
+    except rospy.ROSInterruptExecution:
+        pass
