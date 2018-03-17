850e05804c849b3b97b6eb4ab2338fe5a082ab32
Chu-Sheng Ku <chusheng.ku@colorado.edu>
2018-03-17 03:47:07 +0000

    Delete ignore files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/odroid/Dev/asurada/src
-# Build directory: /home/odroid/Dev/asurada/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
---
+++ src/state_estimate/scripts/state_publisher.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import maestro
+import rospy
+from std_msgs.msg import Float64
+
+CHANNEL_L_IR = 6
+CHANNEL_R_IR = 8
+
+def publisher():
+    pub = rospy.Publisher('state', Float64, queue_size=10)
+    rospy.init_node('state_estimate', anonymous=True)
+    rate = rospy.Rate(10)
+    pololu = maestro.Controller()
+    while not rospy.is_shutdown():
+        left_dist = pololu.getPosition(CHANNEL_L_IR)
+        right_dist = pololu.getPosition(CHANNEL_R_IR)
+        diff = left_dist - right_dist
+        pub.publish(diff)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        publisher()
+    except rospy.ROSInterruptException:
+        pass
