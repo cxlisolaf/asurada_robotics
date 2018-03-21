3dbc971e3288f45de4a774965baabeffa1b637c4
Chu-Sheng Ku <chusheng.ku@colorado.edu>
2018-03-21 21:00:51 +0000

    Modify window size to 10

--- src/state_estimate/scripts/state_publisher.py
+++ src/state_estimate/scripts/state_publisher.py
@@ -6,7 +6,7 @@ from transform import transform
 
 CHANNEL_L_IR = 6
 CHANNEL_R_IR = 11
-WINDOW_SIZE = 50
+WINDOW_SIZE = 10
 
 def publisher():
     pub = rospy.Publisher('state', Float64, queue_size=10)
@@ -18,21 +18,21 @@ def publisher():
     while not rospy.is_shutdown():
         ir_left = pololu.getPosition(CHANNEL_L_IR)
         dist_left = transform(ir_left)
-        print 'ir left: ' + str(ir_left)
-        print 'dist left: ' + str(dist_left)
+        # print 'ir left: ' + str(ir_left)
+        # print 'dist left: ' + str(dist_left)
         ir_right = pololu.getPosition(CHANNEL_R_IR)
         dist_right = transform(ir_right)
-        print 'ir right: ' + str(ir_right)
-        print 'dist right: ' + str(dist_right)
+        # print 'ir right: ' + str(ir_right)
+        # print 'dist right: ' + str(dist_right)
         diff = dist_left - dist_right
         window[index] = diff
         index += 1
         index %= WINDOW_SIZE
         average = sum(window) / WINDOW_SIZE
         pub.publish(average)
-        print 'ir diff: ' + str(ir_left - ir_right)
-        print 'dist diff: ' + str(diff)
-        print 'average: ' + str(average)
+        # print 'ir diff: ' + str(ir_left - ir_right)
+        # print 'dist diff: ' + str(diff)
+        # print 'average: ' + str(average)
         rate.sleep()
 
 if __name__ == '__main__':
