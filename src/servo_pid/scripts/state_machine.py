4e495d4f90164e68357a390167b23e13162c6de6
RyanCheng6608 <rych6608@colorado.edu>
2018-04-27 12:59:48 -0600

    state machine x1

--- src/servo_pid/scripts/state_machine.py
+++ src/servo_pid/scripts/state_machine.py
@@ -24,42 +24,64 @@ control_pub = rospy.Publisher('control_input', Float64, queue_size=10)
 #Going State State
 class Straight(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['corner'])
+        smach.State.__init__(self,
+                                outcomes=['corner'],
+                                input_keys=['ir_in', 'ce_straight'])
 
+        rospy.Subscriber('state', Float64, callback)
         #Pull state information from state_publisher, in form of distance from center in cm
-        self.ir_in = rospy.Subscriber('state', Float64, queue_size=10)
-        self.ce_straight = rospy.Subscriber('ce_straight', Float64, queue_size=10)
 
     def execute(self, userdata):
         #rospy.loginfo('Straight')
-        if self.ir_in > 200:
+        if userdata.ir_in > 200:
             return 'corner'
         else:
-            control_pub.publish(ce_straight)
+            rospy.Subscriber('ce_straight', Float64, cb_straight)
+            rospy.Subscriber('state', Float64, callback)
+            control_pub.publish(userdata.ce_straight)
             rate.sleep()
             print('FULL SPEED AHEAD!')
+
 
 #Going around a corner state
 class Corner(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['straight'])
+        smach.State.__init__(self,
+                                outcomes=['straight'],
+                                input_keys=['ir_in', 'ce_corner'])
 
         #Pyll state information form state_publisher, in form of distance from center in cm
-        self.ir_in = rospy.Subscriber('state', Float64, queue_size=10)
-        self.ce_corner = rospy.Subscriber('ce_corner', Float64, queue_size=10)
+        rospy.Subscriber('ce_corner', Float64, cb_corner)
 
     def execute(self, userdata):
         #rospy.loginfo('Corner')
-        if 75 < ir_in:
-            control_pub.publish(ce_corner)
+        if 75 < userdata.ir_in:
+            rospy.Subscriber('ce_corner', Float64, cb_corner)
+            rospy.Subscriber('state', Float64, callback)
+            control_pub.publish(userdata.ce_corner)
             rate.sleep()
             print('CORNERING!')
         else:
             return 'straight'
 
+def callback(data):
+    userdata.ir_in = data.data
+    return userdata
+
+def cb_straight(data):
+    userdata.ce_straight = data.data
+    return  userdata
+
+def cb_corner(data):
+    userdata.ce_corner = data.data
+    return userdata
+
 def main():
     #Create State SMACH state machine
     sm = smach.StateMachine(outcomes=['base'])
+    sm.userdata.ir_in = 0 #initialize userdata object with attribute ir_in at 0
+    sm.userdata.ce_corner = 0
+    sm.userdata.ce_straight = 0
 
     #Open the State container here
     with sm:
