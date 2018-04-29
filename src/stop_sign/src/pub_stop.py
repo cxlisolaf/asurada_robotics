cdf5392b94bf7aff3dc5ddd5e22dd09f0dec0407
cxlisolaf <doramifa@gmail.com>
2018-04-28 19:17:55 -0700

    change pub_stop.py

--- src/stop_sign/src/pub_stop.py
+++ src/stop_sign/src/pub_stop.py
@@ -18,6 +18,29 @@ from cv_bridge import CvBridge, CvBridgeError
 import os
 import numpy as np
 
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+
+model.load_weights('first_try.h5')
+
 
 def detect(image):
    	
@@ -27,29 +50,6 @@ def detect(image):
 	 input_shape = (3, img_width, img_height)
     else:
 	 input_shape = (img_width, img_height, 3)
-
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    model.add(Flatten())
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
-
-
-    model.load_weights('first_try.h5')
 
     img = load_img(image,False,target_size=(img_width,img_height))
     x = img_to_array(img)
