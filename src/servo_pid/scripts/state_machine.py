b52ef66756ddc848f9626e0502abee7afb9c6967
RyanCheng6608 <rych6608@colorado.edu>
2018-04-28 18:38:45 -0600

    changed from smach frame work to simple if/else

--- src/servo_pid/scripts/state_machine.py
+++ src/servo_pid/scripts/state_machine.py
@@ -5,118 +5,45 @@ import smach
 import smach_ros
 from std_msgs.msg import Float64
 
-rospy.init_node('state_machine', anonymous=True)
+
 global control_pub
 global rate
+ce_corner = 0
+ce_straight = 0
 rate = rospy.Rate(50) #in Hz
-
-# create publisher object and map to "control_pub" variable. Publish to "/control_input"
-# topic which the pololupub.py subscribes to.
-control_pub = rospy.Publisher('control_input', Float64, queue_size=10)
 
 # State input from IR sensor, data in is average distance from center of hallway
 # Negative value = Left of Center
 # Positive value = Right of Center
 
-#Going State State
-class Straight(smach.State):
-    def __init__(self):
-        smach.State.__init__(self,
-                                outcomes=['corner','straight'],
-                                input_keys=['ir_in', 'ce_straight'])
 
-    def execute(self, userdata):
-        #rospy.loginfo('Straight')
-        rospy.Subscriber('state', Float64, self.callback)
-        if userdata.ir_in < -150:
-            return 'corner'
-#            rospy.loginfo('FULL SPEED AHEAD!')
-#            print(userdata.ce_straight)
-        else:
-            rospy.Subscriber('ce_straight', Float64, self.cb_straight)
-            control_pub.publish(userdata.ce_straight)
-            rate.sleep()
-            print(userdata.ir_in)
-            return 'straight'
+def callback(data):
+    if data.data < -200:
+        control_pub.publish(ce_corner)
+        print 'corner!'
+    else
+        control_pub.publish(ce_straight)
+        print 'FULL SPEED AHEAD!'
 
-    def callback(self, data):
-        sm.userdata.ir_in = data.data
-#        rospy.loginfo('Got ir_in callback')
+def cb_straight (data):
+    global ce_straight
+    ce_straight = data.data
 
-    def cb_straight(self, data):
-  #      rospy.loginfo('Got ce_straight callback')
-        sm.userdata.ce_straight = data.data
-
-#        if userdata.ir_in > 200:
-#            return 'corner'
-#        else:
-#            rospy.Subscriber('ce_straight', Float64, cb_straight)
-#            rospy.Subscriber('state', Float64, callback)
-#            control_pub.publish(userdata.ce_straight)
-#            rate.sleep()
-#            print('FULL SPEED AHEAD!')
-#            return 'straight'
-
-
-#Going around a corner state
-class Corner(smach.State):
-    def __init__(self):
-        smach.State.__init__(self,
-                                outcomes=['straight','corner'],
-                                input_keys=['ir_in', 'ce_corner'])
-
-    def execute(self, userdata):
-        #rospy.loginfo('Corner')
-        rospy.Subscriber('state', Float64, self.callback)
-        if userdata.ir_in < -50:
-            rospy.Subscriber('ce_corner', Float64, self.cb_corner)
-            control_pub.publish(userdata.ce_corner)
-            rate.sleep()
-            rospy.loginfo('CORNERING!')
-            return 'corner'
-        else:
-            return 'straight'
-
-    def callback(self, data):
-        sm.userdata.ir_in = data.data
- #       rospy.loginfo('Got ir_in callback')
-
-    def cb_corner(self, data):
- #       rospy.loginfo('Got ce_corner callback')
-        sm.userdata.ce_corner = data.data
-
-#        if 75 < userdata.ir_in:
-#            rospy.Subscriber('ce_corner', Float64, cb_corner)
-#            rospy.Subscriber('state', Float64, callback)
-#            control_pub.publish(userdata.ce_corner)
-#            rate.sleep()
-#            print('CORNERING!')
-#            return 'corner'
-#        else:
-#            return 'straight'
-
+def cb_corner (data):
+    global ce_corner
+    ce_corner =  data.data
 
 def main():
-    #Create State SMACH state machine
-    sm = smach.StateMachine(outcomes=['BASE'])
-    sm.userdata.ir_in = 0 #initialize userdata object with attribute ir_in at 0
-    sm.userdata.ce_corner = 0
-    sm.userdata.ce_straight = 0
-    global sm
-
-    #Open the State container here
-    with sm:
-        #Add States with transitions
-        smach.StateMachine.add('STRAIGHT', Straight(),
-                                transitions={'corner':'CORNER',
-                                            'straight':'STRAIGHT'})
-        smach.StateMachine.add('CORNER', Corner(),
-                                transitions={'straight':'STRAIGHT', 
-                                            'corner':'CORNER'})
-
-    outcome = sm.execute()
-
-
+    #initialize state_machine node
+    rospy.init_node('state_machine', anonymous=True)
+    # create publisher object and map to "control_pub" variable. Publish to "/control_input"
+    # topic which the pololupub.py subscribes to.
+    control_pub = rospy.Publisher('control_input', Float64, queue_size=10)
+    while not rospy.is_shutdown():
+        rospy.Subscriber('ce_straight', Float64, cb_straight)
+        rospy.Subscriber('ce_corner', Float64, cb_corner)
+        rospy.Subscriber('state', Float64, callback)
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
