65a0d82ee4c25fefe4ba5ac51792f120f99cd535
Chu-Sheng Ku <chusheng.ku@colorado.edu>
2018-04-29 18:53:25 +0000

    working state_machine REV 1

--- src/servo_pid/scripts/state_machine.py
+++ src/servo_pid/scripts/state_machine.py
@@ -5,45 +5,46 @@ import smach
 import smach_ros
 from std_msgs.msg import Float64
 
-
-global control_pub
-global rate
-ce_corner = 0
 ce_straight = 0
-rate = rospy.Rate(50) #in Hz
-
+ce_corner = 0
 # State input from IR sensor, data in is average distance from center of hallway
 # Negative value = Left of Center
 # Positive value = Right of Center
 
 
 def callback(data):
+    print data.data
     if data.data < -200:
         control_pub.publish(ce_corner)
         print 'corner!'
-    else
+    else:
         control_pub.publish(ce_straight)
         print 'FULL SPEED AHEAD!'
 
-def cb_straight (data):
+def cb_straight(data):
+    print data.data
     global ce_straight
     ce_straight = data.data
 
-def cb_corner (data):
+def cb_corner(data):
+    print data.data
     global ce_corner
-    ce_corner =  data.data
+    ce_corner = data.data
 
 def main():
     #initialize state_machine node
     rospy.init_node('state_machine', anonymous=True)
     # create publisher object and map to "control_pub" variable. Publish to "/control_input"
     # topic which the pololupub.py subscribes to.
+    global control_pub
     control_pub = rospy.Publisher('control_input', Float64, queue_size=10)
-    while not rospy.is_shutdown():
-        rospy.Subscriber('ce_straight', Float64, cb_straight)
-        rospy.Subscriber('ce_corner', Float64, cb_corner)
-        rospy.Subscriber('state', Float64, callback)
-        rate.sleep()
+    # rate = rospy.Rate(100)
+    rospy.Subscriber('ce_straight', Float64, cb_straight)
+    rospy.Subscriber('ce_corner', Float64, cb_corner)
+    rospy.Subscriber('state', Float64, callback)
+    print ce_corner, ce_straight
+    # rate.sleep()
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
