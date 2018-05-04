5dedbdfb7b48d795dfad9106261f83c6bc8196f1
cxlisolaf <doramifa@gmail.com>
2018-05-04 07:38:41 -0700

    fix error

--- src/stop_sign/src/test_network.py
+++ src/stop_sign/src/test_network.py
@@ -3,12 +3,17 @@
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
-import argparse
+
 import imutils
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
+import rospy
 
-model = load_model("stop_sign2.model)
+from sensor_msgs.msg import Image
+from std_msgs.msg import Bool
+import maestro
+
+model = load_model("stopsign_2.model")
 
 def listener():
 
@@ -31,31 +36,31 @@ def callback(data):
 
         if counter%5 == 0:
 
-	image = cv2.imread("frame.jpg")
+	    image = cv2.imread("frame.jpg")
 
-	# pre-process the image for classification
-	image = cv2.resize(image, (28, 28))
-	image = image.astype("float") / 255.0
-	image = img_to_array(image)
-	image = np.expand_dims(image, axis=0)
+	    # pre-process the image for classification
+	    image = cv2.resize(image, (28, 28))
+	    image = image.astype("float") / 255.0
+	    image = img_to_array(image)
+	    image = np.expand_dims(image, axis=0)
 
-	# load the trained convolutional neural network
-	print "[INFO] loading network..."
+	    # load the trained convolutional neural network
+	    print "[INFO] loading network..."
 
-	# classify the input image
-	(nonstop, stop) = model.predict(image)[0]
+	    # classify the input image
+	    (nonstop, stop) = model.predict(image)[0]
 
-	# build the label
-	label = "stop" if stop > nonstop else "nonstop"
-	proba = stop if stop > nonstop else nonstop
-	label = "{}: {:.2f}%".format(label, proba * 100)
+	    # build the label
+	    label = "stop" if stop > nonstop else "nonstop"
+	    proba = stop if stop > nonstop else nonstop
+	    label = "{}: {:.2f}%".format(label, proba * 100)
 
-	print label
+	    print label
 
-        if label == "stop":
-            maestro.Controller().setTarget(1,3000)
-        else:
-            maestro.Controller().setTarget(1,6500)
+            if label == "stop":
+                maestro.Controller().setTarget(1,3000)
+            else:
+                maestro.Controller().setTarget(1,6500)
 
 
     except CvBridgeError as e:
